f66b4c77a8bef0c1a5c1c68785a1dd2ba767393e
Tom Hennessy <tomlhennessy@outlook.com>
2024-11-19 12:43:38 +1000

    about me

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function AboutMe() {
+  return (
+    <div className='flex flex-col md:grid md:grid-cols-2 md:col-span-3'>
+      <div className='flex flex-col gap-3'>
+        <div className='flex items-center'>
+            <h1 className='font-bold text-4xl sm:text-6xl'>Tom Hennessy</h1>
+            <img className='mx-auto md:hidden h-[200px] aspect-square rounded-full shadow-lg' src='https://media.licdn.com/dms/image/v2/D4E03AQFXNylXUxQfGA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731983470996?e=1737590400&v=beta&t=hHafkOWBsy3b1jWesWkgNidVQ39siEyQL6o9m6qXq04' alt='me' />
+        </div>
+        <h2 className='font-medium text-lg text-slate-800 sm:text-2xl'>Software Engineer</h2>
+        <p className='text-slate-500'>I absolutely love to create fun and interesting sites using the latest and greatest technology.</p>
+        <div className='my-10 flex items-center gap-4 text-sm sm:text-base'>
+          <button className='rounded-full bg-gradient-to-r from-blue-700 to-violet-700 text-white hover:opacity-70 px-4 py-2'>projects</button>
+          <button className='rounded-full text-purple-800 border-solid border-violet-800 hover:opacity-70 px-4 py-2'>contact</button>
+        </div>
+      </div>
+      <img className='mx-auto hidden md:flex h-[200px] aspect-square rounded-full shadow-lg' src='https://media.licdn.com/dms/image/v2/D4E03AQFXNylXUxQfGA/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1731983470996?e=1737590400&v=beta&t=hHafkOWBsy3b1jWesWkgNidVQ39siEyQL6o9m6qXq04' alt='me' />
+    </div>
+  )
+}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,7 +1,14 @@
 import React from 'react'
+import AboutMe from './AboutMe'
+import Experience from './Experience'
+import Skills from './Skills'
 
 export default function Main() {
   return (
-    <div>Main</div>
+    <div className='p-4 flex flex-col md:grid md:grid-cols-4'>
+      <AboutMe />
+      <Experience />
+      <Skills />
+    </div>
   )
 }
